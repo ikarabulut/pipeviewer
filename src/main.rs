b654c5288a9472969a906f08a9d9b114fee42634
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-06 20:55:19 -0500

    add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+use std::env;
 use std::io::{self, Read, Write};
+use clap::{Command, Arg};
 
 const CHUNK_SIZE: usize = 6 * 1024;
 
 fn main() {
     let mut total_bytes = 0;
+    let matches = Command::new("pipeviewer")
+        .arg(Arg::new("infile").help("Read a file instead of stdin"))
+        .arg(
+            Arg::new("outfile")
+            .short('o')
+            .long("outfile")
+            .value_name("Outfile")
+            .help("Write output to a file instead of stdout")
+            .action(clap::ArgAction::Set)  // Specify that this argument takes a value
+        )
+        .arg(Arg::new("silent")
+            .short('s')
+            .long("silent")
+            .action(clap::ArgAction::SetTrue))  // Specify that this argument is a flag
+        .get_matches();
+
+    let infile = matches.get_one::<String>("infile").map(|s| s.as_str()).unwrap_or_default();
+    let outfile = matches.get_one::<String>("outfile").map(|s| s.as_str()).unwrap_or_default();
+    let silent = if matches.get_flag("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
     loop {
         let mut buffer = [0; CHUNK_SIZE];
 
