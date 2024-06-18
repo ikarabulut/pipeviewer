8a83bc114cefc5f02b860026f457763a5ad39818
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-18 14:29:45 -0500

    Refactor to use crossbeam channels

--- src/read.rs
+++ src/read.rs
@@ -1,9 +1,13 @@
 use crate::CHUNK_SIZE;
+use crossbeam::channel::Sender;
 use std::fs::File;
 use std::io::{self, BufReader, Error, Read};
-use std::sync::mpsc::Sender;
 
-pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<(), Error> {
+pub fn read_loop(
+    infile: &str,
+    stats_tx: Sender<usize>,
+    write_tx: Sender<Vec<u8>>,
+) -> Result<(), Error> {
     let mut reader: Box<dyn Read> = if !infile.is_empty() {
         Box::new(BufReader::new(File::open(infile)?))
     } else {
@@ -17,10 +21,13 @@ pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<(), Error> {
             Ok(x) => x,
             Err(_) => break,
         };
-        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+        let _ = stats_tx.send(num_read);
+        if write_tx.send(Vec::from(&buffer[..num_read])).is_err() {
             break;
         }
     }
-    let _ = stats_tx.send(Vec::new());
+    let _ = stats_tx.send(0);
+    let _ = write_tx.send(Vec::new());
+
     Ok(())
 }
